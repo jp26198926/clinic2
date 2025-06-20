57701e14d97bdd85f6a489432bba3c821af33b1f
Jaypee Hindang <jp@upliftcodecamp.com>
2025-06-20 11:32:33 +1000

    fix: current_transaction_view: modal lab upload make the modal_lab size to medium

--- application/views/current_transaction/modal_lab.php
+++ application/views/current_transaction/modal_lab.php
@@ -1,6 +1,6 @@
 <!-- Lab Result Modal -->
 <div id='modal_lab' class="modal fade" tabindex="-1" role="dialog">
-    <div class="modal-dialog modal-xl" role="document">
+    <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal">&times;</button>
